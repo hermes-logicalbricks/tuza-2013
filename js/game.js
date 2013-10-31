d19658a9dfed199a7b4b6329a73730e6c7686584
hermes-logicalbricks <hermes.ojeda@logicalbricks.com>
2013-10-31 07:46:36 -0600

    Working movement on the map without constrains

--- js/game.js
+++ js/game.js
@@ -1,50 +1,77 @@
 function PlayState() {
+  var viewport;
+  var raw_terrain, raw_pixeldata;
+  var terrain;
+
+
+
   var player;
   var goal;
 
 
   this.setup = function() {
-    player = new jaws.Sprite({ image: "images/bullet.png", x: 20, y: 20 });
-    goal = new jaws.Sprite({ image: "images/block.bmp", x: 80, y: 80 });
+    // Configurando todo
+    jaws.context.mozImageSmoothingEnabled = false;
+    jaws.preventDefaultKeys(["w","a", "s","d","space","z","up","down","right","left"])
+
+    // Iniciando el terreno
+    terrain = new jaws.Sprite({x: 0, y: 0, image: 'images/level1.png'});
+    raw_terrain = terrain.asCanvasContext();
+    raw_pixeldata = raw_terrain.getImageData(0, 0, terrain.width, terrain.height).data;
+
+    viewport = new jaws.Viewport({max_x: terrain.width, max_y: terrain.height});
+
+    start_pos = [30*8,240*8];
+
+    player = new jaws.Sprite({ x: 20, y: 20, anchor: "center_bottom" });
+    player.animation = new jaws.Animation({ sprite_sheet: 'images/tuza_sprite2.png', frame_size: [40,52], frame_duration: 120, subsets: { saltar: [2,4] } });
+
+    player.move_anim = player.animation.slice(0,2);
+    player.vx = player.vy = 0;
+    player.x = start_pos[0];
+    player.y = start_pos[1];
   };
 
   this.update = function() {
-    player.setImage("images/bullet.png");
-
     if(jaws.pressed("up")) {
-      player.y -= 4;
+      player.y -= 2;
+      player.setImage(player.animation.subset('saltar').next());
     }
 
     if(jaws.pressed("left")) {
-      player.x -= 4;
+      player.x -= 2;
+      player.setImage(player.move_anim.next());
     }
 
     if(jaws.pressed("down")) {
-      player.y += 4;
+      player.y += 2;
+      player.setImage(player.animation.subset('saltar').next());
     }
 
     if(jaws.pressed("right")) {
-      player.x += 4;
+      player.x += 2;
+      player.setImage( player.move_anim.next());
     }
 
-    if(jaws.collideOneWithOne(player, goal)) {
-      player.setImage("images/bullet1.png");
-    }
+    viewport.x = player.x - jaws.width / 2
+    viewport.y = player.y - jaws.height + 100
 
   };
 
 
   this.draw = function() {
-    jaws.clear();
-    player.draw();
-    goal.draw();
+    jaws.clear()
+
+    viewport.apply( function() {
+      terrain.draw()
+      player.draw()
+    });
   };
 
 };
 
 jaws.onload = function() {
-  jaws.assets.add("images/block.bmp");
-  jaws.assets.add("images/bullet.png");
-  jaws.assets.add("images/bullet1.png");
+  jaws.assets.add("images/level1.png");
+  jaws.assets.add("images/tuza_sprite2.png");
   jaws.start(PlayState);
 };
